629b396129fbeced11dc1c148e310206b11f7abd
LMesaric <luka.mesaric@fer.hr>
2021-12-01 11:39:05 +0100

    Add solution for day 1

---
+++ day01/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+std::vector<int32_t> parseInput() {
+    std::vector<int32_t> depths;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        depths.push_back(std::stoi(line));
+    }
+    return depths;
+}
+
+int32_t countIncreasing(std::vector<int32_t> &depths, uint32_t dist) {
+    int32_t cnt = 0;
+    for (auto i = dist; i < depths.size(); ++i)
+        cnt += depths[i] > depths[i - dist];
+    return cnt;
+}
+
+int32_t countIncreasingSingle(std::vector<int32_t> &depths) {
+    return countIncreasing(depths, 1);
+}
+
+int32_t countIncreasingSliding(std::vector<int32_t> &depths) {
+    return countIncreasing(depths, 3);
+}
+
+int main() {
+    std::vector<int32_t> input = parseInput();
+
+    int32_t countOne = countIncreasingSingle(input);
+    assert(countOne == 1316);
+    std::cout << countOne << std::endl;
+
+    int32_t countTwo = countIncreasingSliding(input);
+    assert(countTwo == 1344);
+    std::cout << countTwo << std::endl;
+}
